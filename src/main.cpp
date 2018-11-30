24e97765a1052eb297c983b21614424db4ea0eae
Antonio Vanegas <hpsaturn@gmail.com>
2018-11-30 11:52:16 +0100

    succesuful initial test ready with 'DHT sensor library for ESPx' library

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,179 @@
-//
-//    FILE: dht22_test_noIRQ.ino
-//  AUTHOR: Rob Tillaart
-// VERSION: 0.1.0
-// PURPOSE: DHT library test sketch for DHT22 && Arduino
-//     URL:
-// HISTORY:
-// 0.1.0 initial version
-//
-// Released to the public domain
-//
+#include "DHTesp.h"
+#include "Ticker.h"
 
-#include <dht.h>
+#ifndef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP32 ONLY!)
+#error Select ESP32 board.
+#endif
 
-dht DHT;
+/**************************************************************/
+/* Example how to read DHT sensors from an ESP32 using multi- */
+/* tasking.                                                   */
+/* This example depends on the ESP32Ticker library to wake up */
+/* the task every 20 seconds                                  */
+/* Please install Ticker-esp32 library first                  */
+/* bertmelis/Ticker-esp32                                     */
+/* https://github.com/bertmelis/Ticker-esp32                  */
+/**************************************************************/
 
-#define DHT22_PIN 5
+DHTesp dht;
 
-struct
-{
-    uint32_t total;
-    uint32_t ok;
-    uint32_t crc_error;
-    uint32_t time_out;
-    uint32_t connect;
-    uint32_t ack_l;
-    uint32_t ack_h;
-    uint32_t unknown;
-    uint32_t sumtime;
-    uint32_t mintime;
-} stat = { 0,0,0,0,0,0,0,0, 0, 9999};
+void tempTask(void *pvParameters);
+bool getTemperature();
+void triggerGetTemp();
+
+/** Task handle for the light value read task */
+TaskHandle_t tempTaskHandle = NULL;
+/** Ticker for temperature reading */
+Ticker tempTicker;
+/** Comfort profile */
+ComfortState cf;
+/** Flag if task should run */
+bool tasksEnabled = false;
+/** Pin number for DHT11 data pin */
+int dhtPin = 17;
+
+/**
+ * initTemp
+ * Setup DHT library
+ * Setup task and timer for repeated measurement
+ * @return bool
+ *    true if task and timer are started
+ *    false if task or timer couldn't be started
+ */
+bool initTemp() {
+  byte resultValue = 0;
+  // Initialize temperature sensor
+	dht.setup(dhtPin, DHTesp::DHT22);
+	Serial.println("DHT initiated");
+
+  // Start task to get temperature
+	xTaskCreatePinnedToCore(
+			tempTask,                       /* Function to implement the task */
+			"tempTask ",                    /* Name of the task */
+			4000,                           /* Stack size in words */
+			NULL,                           /* Task input parameter */
+			5,                              /* Priority of the task */
+			&tempTaskHandle,                /* Task handle. */
+			1);                             /* Core where the task should run */
+
+  if (tempTaskHandle == NULL) {
+    Serial.println("Failed to start task for temperature update");
+    return false;
+  } else {
+    // Start update of environment data every 20 seconds
+    tempTicker.attach(5, triggerGetTemp);
+  }
+  return true;
+}
+
+/**
+ * triggerGetTemp
+ * Sets flag dhtUpdated to true for handling in loop()
+ * called by Ticker getTempTimer
+ */
+void triggerGetTemp() {
+  if (tempTaskHandle != NULL) {
+	   xTaskResumeFromISR(tempTaskHandle);
+  }
+}
+
+/**
+ * Task to reads temperature from DHT11 sensor
+ * @param pvParameters
+ *    pointer to task parameters
+ */
+void tempTask(void *pvParameters) {
+	Serial.println("tempTask loop started");
+	while (1) // tempTask loop
+  {
+    if (tasksEnabled) {
+      // Get temperature values
+			getTemperature();
+		}
+    // Got sleep again
+		vTaskSuspend(NULL);
+	}
+}
+
+/**
+ * getTemperature
+ * Reads temperature from DHT11 sensor
+ * @return bool
+ *    true if temperature could be aquired
+ *    false if aquisition failed
+*/
+bool getTemperature() {
+	// Reading temperature for humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+	// Check if any reads failed and exit early (to try again).
+	if (dht.getStatus() != 0) {
+		Serial.println("DHT11 error status: " + String(dht.getStatusString()));
+		return false;
+	}
+
+	float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
+  float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
+  float cr = dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
+
+  String comfortStatus;
+  switch(cf) {
+    case Comfort_OK:
+      comfortStatus = "Comfort_OK";
+      break;
+    case Comfort_TooHot:
+      comfortStatus = "Comfort_TooHot";
+      break;
+    case Comfort_TooCold:
+      comfortStatus = "Comfort_TooCold";
+      break;
+    case Comfort_TooDry:
+      comfortStatus = "Comfort_TooDry";
+      break;
+    case Comfort_TooHumid:
+      comfortStatus = "Comfort_TooHumid";
+      break;
+    case Comfort_HotAndHumid:
+      comfortStatus = "Comfort_HotAndHumid";
+      break;
+    case Comfort_HotAndDry:
+      comfortStatus = "Comfort_HotAndDry";
+      break;
+    case Comfort_ColdAndHumid:
+      comfortStatus = "Comfort_ColdAndHumid";
+      break;
+    case Comfort_ColdAndDry:
+      comfortStatus = "Comfort_ColdAndDry";
+      break;
+    default:
+      comfortStatus = "Unknown:";
+      break;
+  };
+
+  Serial.println(" T:" + String(newValues.temperature) + " H:" + String(newValues.humidity) + " I:" + String(heatIndex) + " D:" + String(dewPoint) + " " + comfortStatus);
+	return true;
+}
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("dht22_test.ino");
-    Serial.print("LIBRARY VERSION: ");
-    Serial.println(DHT_LIB_VERSION);
-    Serial.println();
-    
-    Serial.print("IRQ:\t");
-    Serial.println( DHT.getDisableIRQ() );
-    DHT.setDisableIRQ(true);
-    Serial.print("IRQ:\t");
-    Serial.println( DHT.getDisableIRQ() );
-    
-    Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("DHT ESP32 example with tasks");
+  initTemp();
+  // Signal end of setup() to tasks
+  tasksEnabled = true;
 }
 
-void loop()
-{
-    // READ DATA
-    Serial.print("DHT22, \t");
-
-    uint32_t start = micros();
-    int chk = DHT.read22(DHT22_PIN);
-    uint32_t stop = micros();
-
-    stat.total++;
-    switch (chk)
-    {
-    case DHTLIB_OK:
-        stat.ok++;
-        Serial.print("OK,\t");
-        break;
-    case DHTLIB_ERROR_CHECKSUM:
-        stat.crc_error++;
-        Serial.print("Checksum error,\t");
-        break;
-    case DHTLIB_ERROR_TIMEOUT:
-        stat.time_out++;
-        Serial.print("Time out error,\t");
-        break;
-    case DHTLIB_ERROR_CONNECT:
-        stat.connect++;
-        Serial.print("Connect error,\t");
-        break;
-    case DHTLIB_ERROR_ACK_L:
-        stat.ack_l++;
-        Serial.print("Ack Low error,\t");
-        break;
-    case DHTLIB_ERROR_ACK_H:
-        stat.ack_h++;
-        Serial.print("Ack High error,\t");
-        break;
-    default:
-        stat.unknown++;
-        Serial.print("Unknown error,\t");
-        break;
-    }
-    // DISPLAY DATA
-    Serial.print(DHT.humidity, 1);
-    Serial.print(",\t");
-    Serial.print(DHT.temperature, 1);
-    Serial.print(",\t");
-    uint32_t duration = stop - start;
-    stat.sumtime += duration;
-    stat.mintime = min(stat.mintime, duration);
-    Serial.print(duration);
-    Serial.println();
-
-    if (stat.total % 20 == 0)
-    {
-        Serial.println("\nTOT\tOK\tCRC\tTIMO\tCON\tACKL\tACKH\tAVG\MIN");
-        Serial.print(stat.total);
-        Serial.print("\t");
-        Serial.print(stat.ok);
-        Serial.print("\t");
-        Serial.print(stat.crc_error);
-        Serial.print("\t");
-        Serial.print(stat.time_out);
-        Serial.print("\t");
-        Serial.print(stat.connect);
-        Serial.print("\t");
-        Serial.print(stat.ack_l);
-        Serial.print("\t");
-        Serial.print(stat.ack_h);
-        Serial.print("\t");
-        Serial.print(stat.unknown);
-        Serial.print("\t");
-        Serial.print(1.0 * stat.sumtime / stat.total);
-        Serial.print("\t");
-        Serial.print(1.0 * stat.mintime);
-        Serial.println("\n");
-    }
-    delay(500);
+void loop() {
+  if (!tasksEnabled) {
+    // Wait 2 seconds to let system settle down
+    delay(2000);
+    // Enable task that will read values from the DHT sensor
+    tasksEnabled = true;
+    if (tempTaskHandle != NULL) {
+			vTaskResume(tempTaskHandle);
+		}
+  }
+  yield();
 }
-//
-// END OF FILE
-//
