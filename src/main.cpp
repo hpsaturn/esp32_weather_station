2297c53c33147dac9940e9fadd0a6534a49f8310
Antonio Vanegas <hpsaturn@gmail.com>
2021-02-19 20:16:08 +0100

    fixed some pio issues and clang format issues

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,23 @@
  * rev003 20181203 added blink led on BLE waiting connection state
  ********************************************************************/
 
-#include "DHTesp.h"
-#include "Ticker.h"
+#include <BLE2902.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
+
+#include "DHTesp.h"
+#include "Ticker.h"
 #include "esp_sleep.h"
 
 #ifndef ESP32
-#pragma message(THIS CODE IS FOR ESP32 ONLY!)
+#pragma message(THIS CODE IS FOR ESP32 ONLY !)
 #error Select ESP32 board.
 #endif
 
 DHTesp dht;
 
-void tempTask(void *pvParameters);
+void tempTask(void* pvParameters);
 bool getTemperature();
 void triggerGetTemp();
 
@@ -42,19 +43,19 @@ ComfortState cf;
 /** Flag if task should run */
 bool tasksEnabled = true;
 /** Pin number for DHT22 data pin */
-int dhtPin = 16;
+int dhtPin = 2;
 
 // Bluetooth variables
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharactDHT22 = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-#define SERVICE_UUID        "c8d1d262-861f-5082-947e-f383a259aadd"
-#define CHARAC_DHT_UUID    "b0f332a8-a5aa-4f3a-bb43-f99e8811ae01"
+#define SERVICE_UUID "c8d1d262-861f-5082-947e-f383a259aadd"
+#define CHARAC_DHT_UUID "b0f332a8-a5aa-4f3a-bb43-f99e8811ae01"
 
 // ESP32 Deep Sleep time
-#define DEEP_SLEEP_DURATION 15 // sleep x seconds and then wake up
-#define GPIO_LED_GREEN 22 // Led on TTGO board (black) 
+#define DEEP_SLEEP_DURATION 15  // sleep x seconds and then wake up
+#define GPIO_LED_GREEN 22       // Led on TTGO board (black)
 #define GPIO_SENSOR_ENABLE 17
 
 /**
@@ -62,10 +63,10 @@ bool oldDeviceConnected = false;
  * notify each data send package
  */
 
-void blinkOnboardLed () { 
-  digitalWrite (GPIO_LED_GREEN, LOW);
-  delay(50); // fast blink for low power
-  digitalWrite (GPIO_LED_GREEN, HIGH);
+void blinkOnboardLed() {
+    digitalWrite(GPIO_LED_GREEN, LOW);
+    delay(50);  // fast blink for low power
+    digitalWrite(GPIO_LED_GREEN, HIGH);
 }
 
 /**
@@ -77,27 +78,27 @@ void blinkOnboardLed () {
  *    false if task or timer couldn't be started
  */
 bool initTemp() {
-  // Initialize temperature sensor
-	dht.setup(dhtPin, DHTesp::DHT22);
-	Serial.println("-->[DHT] initiated");
-  // Start task to get temperature
-	xTaskCreatePinnedToCore(
-			tempTask,                       /* Function to implement the task */
-			"tempTask ",                    /* Name of the task */
-			4000,                           /* Stack size in words */
-			NULL,                           /* Task input parameter */
-			5,                              /* Priority of the task */
-			&tempTaskHandle,                /* Task handle. */
-			1);                             /* Core where the task should run */
+    // Initialize temperature sensor
+    dht.setup(dhtPin, DHTesp::AM2302);
+    Serial.println("-->[DHT] initiated");
+    // Start task to get temperature
+    xTaskCreatePinnedToCore(
+        tempTask,        /* Function to implement the task */
+        "tempTask ",     /* Name of the task */
+        4000,            /* Stack size in words */
+        NULL,            /* Task input parameter */
+        5,               /* Priority of the task */
+        &tempTaskHandle, /* Task handle. */
+        1);              /* Core where the task should run */
 
-  if (tempTaskHandle == NULL) {
-    Serial.println("--->[E] Failed to start task for temperature update");
-    return false;
-  } else {
-    // Start update of environment data every 10 seconds
-    tempTicker.attach(10, triggerGetTemp);
-  }
-  return true;
+    if (tempTaskHandle == NULL) {
+        Serial.println("--->[E] Failed to start task for temperature update");
+        return false;
+    } else {
+        // Start update of environment data every 10 seconds
+        tempTicker.attach(10, triggerGetTemp);
+    }
+    return true;
 }
 
 /**
@@ -106,9 +107,9 @@ bool initTemp() {
  * called by Ticker getTempTimer
  */
 void triggerGetTemp() {
-  if (tempTaskHandle != NULL) {
-	   xTaskResumeFromISR(tempTaskHandle);
-  }
+    if (tempTaskHandle != NULL) {
+        xTaskResumeFromISR(tempTaskHandle);
+    }
 }
 
 /**
@@ -116,17 +117,17 @@ void triggerGetTemp() {
  * @param pvParameters
  *    pointer to task parameters
  */
-void tempTask(void *pvParameters) {
-	Serial.println("-->[DHT] temp task loop started");
-	while (1) // tempTask loop
-  {
-    if (tasksEnabled) {
-      // Get temperature values
-			getTemperature();
-		}
-    // Got sleep again
-		vTaskSuspend(NULL);
-	}
+void tempTask(void* pvParameters) {
+    Serial.println("-->[DHT] temp task loop started");
+    while (1)  // tempTask loop
+    {
+        if (tasksEnabled) {
+            // Get temperature values
+            getTemperature();
+        }
+        // Got sleep again
+        vTaskSuspend(NULL);
+    }
 }
 
 /**
@@ -137,169 +138,166 @@ void tempTask(void *pvParameters) {
  *    false if aquisition failed
 */
 bool getTemperature() {
-	// Reading temperature for humidity takes about 250 milliseconds!
-	// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-  TempAndHumidity newValues = dht.getTempAndHumidity();
-	// Check if any reads failed and exit early (to try again).
-	if (dht.getStatus() != 0) {
-		Serial.println("--->[E] DHT22 error status: " + String(dht.getStatusString()));
-		return false;
-	}
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    TempAndHumidity newValues = dht.getTempAndHumidity();
+    // Check if any reads failed and exit early (to try again).
+    if (dht.getStatus() != 0) {
+        Serial.println("--->[E] DHT22 error status: " + String(dht.getStatusString()));
+        return false;
+    }
 
-	float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
-  float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
-  dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
+    float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
+    float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
+    dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
 
-  String comfortStatus;
-  switch(cf) {
-    case Comfort_OK:
-      comfortStatus = "Comfort_OK";
-      break;
-    case Comfort_TooHot:
-      comfortStatus = "Comfort_TooHot";
-      break;
-    case Comfort_TooCold:
-      comfortStatus = "Comfort_TooCold";
-      break;
-    case Comfort_TooDry:
-      comfortStatus = "Comfort_TooDry";
-      break;
-    case Comfort_TooHumid:
-      comfortStatus = "Comfort_TooHumid";
-      break;
-    case Comfort_HotAndHumid:
-      comfortStatus = "Comfort_HotAndHumid";
-      break;
-    case Comfort_HotAndDry:
-      comfortStatus = "Comfort_HotAndDry";
-      break;
-    case Comfort_ColdAndHumid:
-      comfortStatus = "Comfort_ColdAndHumid";
-      break;
-    case Comfort_ColdAndDry:
-      comfortStatus = "Comfort_ColdAndDry";
-      break;
-    default:
-      comfortStatus = "Unknown:";
-      break;
-  };
+    String comfortStatus;
+    switch (cf) {
+        case Comfort_OK:
+            comfortStatus = "Comfort_OK";
+            break;
+        case Comfort_TooHot:
+            comfortStatus = "Comfort_TooHot";
+            break;
+        case Comfort_TooCold:
+            comfortStatus = "Comfort_TooCold";
+            break;
+        case Comfort_TooDry:
+            comfortStatus = "Comfort_TooDry";
+            break;
+        case Comfort_TooHumid:
+            comfortStatus = "Comfort_TooHumid";
+            break;
+        case Comfort_HotAndHumid:
+            comfortStatus = "Comfort_HotAndHumid";
+            break;
+        case Comfort_HotAndDry:
+            comfortStatus = "Comfort_HotAndDry";
+            break;
+        case Comfort_ColdAndHumid:
+            comfortStatus = "Comfort_ColdAndHumid";
+            break;
+        case Comfort_ColdAndDry:
+            comfortStatus = "Comfort_ColdAndDry";
+            break;
+        default:
+            comfortStatus = "Unknown:";
+            break;
+    };
 
-  Serial.println(
-    "-->[DHT] T:" + String(newValues.temperature) + 
-    " H:" + String(newValues.humidity) + 
-    " I:" + String(heatIndex) + 
-    " D:" + String(dewPoint) + 
-    " " + comfortStatus
-  );
+    Serial.println(
+        "-->[DHT] T:" + String(newValues.temperature) +
+        " H:" + String(newValues.humidity) +
+        " I:" + String(heatIndex) +
+        " D:" + String(dewPoint) +
+        " " + comfortStatus);
 
-  // notify changed value
-  if (deviceConnected) {
-    String output = String("") + String(newValues.temperature) + ";";
-    output = output + String(newValues.humidity) + ";";
-    output = output + String(dewPoint) + ";";
-    output = output + String(heatIndex);
-    pCharactDHT22->setValue(output.c_str());
-    pCharactDHT22->notify();
-  }
+    // notify changed value
+    if (deviceConnected) {
+        String output = String("") + String(newValues.temperature) + ";";
+        output = output + String(newValues.humidity) + ";";
+        output = output + String(dewPoint) + ";";
+        output = output + String(heatIndex);
+        pCharactDHT22->setValue(output.c_str());
+        pCharactDHT22->notify();
+    }
 
-  blinkOnboardLed(); // notify via LED
+    blinkOnboardLed();  // notify via LED
 
-  return true;
+    return true;
 }
 
-void enableSensor () {  // for show receive connections state
-  digitalWrite (GPIO_SENSOR_ENABLE, HIGH);
+void enableSensor() {  // for show receive connections state
+    digitalWrite(GPIO_SENSOR_ENABLE, HIGH);
 }
 
-void disableSensor () {  // for show receive connections state
-  digitalWrite (GPIO_SENSOR_ENABLE, HIGH);
+void disableSensor() {  // for show receive connections state
+    digitalWrite(GPIO_SENSOR_ENABLE, HIGH);
 }
 
-void gotToSuspend (){
-  Serial.println("-->[ESP] suspending..");
-  pServer->getAdvertising()->stop();
-  disableSensor();
-  delay(8); // waiting for writing msg on serial
-  //esp_deep_sleep(1000000LL * DEEP_SLEEP_DURATION);
-  esp_sleep_enable_timer_wakeup(1000000LL * DEEP_SLEEP_DURATION);
-  esp_deep_sleep_start();
+void gotToSuspend() {
+    Serial.println("-->[ESP] suspending..");
+    pServer->getAdvertising()->stop();
+    disableSensor();
+    delay(8);  // waiting for writing msg on serial
+    //esp_deep_sleep(1000000LL * DEEP_SLEEP_DURATION);
+    esp_sleep_enable_timer_wakeup(1000000LL * DEEP_SLEEP_DURATION);
+    esp_deep_sleep_start();
 }
 
 /******************************************************************************
 *   B L U E T O O T H  M E T H O D S
 ******************************************************************************/
-class MyServerCallbacks: public BLEServerCallbacks {
-	void onConnect(BLEServer* pServer) {
-      Serial.println("-->[BLE] onConnect");
-      deviceConnected = true;
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        Serial.println("-->[BLE] onConnect");
+        deviceConnected = true;
     };
 
     void onDisconnect(BLEServer* pServer) {
-      Serial.println("-->[BLE] onDisconnect");
-      deviceConnected = false;
+        Serial.println("-->[BLE] onDisconnect");
+        deviceConnected = false;
     };
-}; // BLEServerCallbacks
+};  // BLEServerCallbacks
 
-void bleServerInit(){
-  // Create the BLE Device
-  BLEDevice::init("ESP32_DTH22");
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  // Create a BLE Characteristic for PM 2.5
-  pCharactDHT22 = pService->createCharacteristic(
-                      CHARAC_DHT_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
-  // Create a BLE Descriptor
-  pCharactDHT22->addDescriptor(new BLE2902());
-  // Start the service
-  pService->start();
-  // Start advertising
-  Serial.println("-->[BLE] start advertising");
-  pServer->getAdvertising()->start();
-  Serial.println("-->[BLE] GATT server ready. (Waiting a client to notify)");
+void bleServerInit() {
+    // Create the BLE Device
+    BLEDevice::init("ESP32_DTH22");
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+    // Create the BLE Service
+    BLEService* pService = pServer->createService(SERVICE_UUID);
+    // Create a BLE Characteristic for PM 2.5
+    pCharactDHT22 = pService->createCharacteristic(
+        CHARAC_DHT_UUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_NOTIFY);
+    // Create a BLE Descriptor
+    pCharactDHT22->addDescriptor(new BLE2902());
+    // Start the service
+    pService->start();
+    // Start advertising
+    Serial.println("-->[BLE] start advertising");
+    pServer->getAdvertising()->start();
+    Serial.println("-->[BLE] GATT server ready. (Waiting a client to notify)");
 }
 
-void bleLoop(){
-  // disconnecting
-  if (!deviceConnected && oldDeviceConnected) {
-    oldDeviceConnected = deviceConnected;
-    // not devices, go to suspend cycle
-    gotToSuspend();
-  }
-  // connecting
-  if (deviceConnected && !oldDeviceConnected)
-  {
-    oldDeviceConnected = deviceConnected;
-    enableSensor();
-    delay(1000);  // waiting for initial capture data
-    initTemp();
-  }
+void bleLoop() {
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        oldDeviceConnected = deviceConnected;
+        // not devices, go to suspend cycle
+        gotToSuspend();
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        oldDeviceConnected = deviceConnected;
+        enableSensor();
+        delay(1000);  // waiting for initial capture data
+        initTemp();
+    }
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("==>[DHT22 ESP32]<==");
-  // GPIO setup
-  pinMode(GPIO_LED_GREEN, OUTPUT);
-  digitalWrite (GPIO_LED_GREEN, HIGH);
-  pinMode(GPIO_SENSOR_ENABLE, OUTPUT);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("==>[DHT22 ESP32]<==");
+    // GPIO setup
+    pinMode(GPIO_LED_GREEN, OUTPUT);
+    digitalWrite(GPIO_LED_GREEN, LOW);
+    pinMode(GPIO_SENSOR_ENABLE, OUTPUT);
 
-  // waiting 1s for connections
-  bleServerInit(); 
-  delay(1000);
+    // waiting 1s for connections
+    bleServerInit();
+    delay(5000);
 
-  // if any device, print sensor data via serial and go to suspend mode;  
-  if(!deviceConnected){
-    gotToSuspend();
-  }
+    // if any device, print sensor data via serial and go to suspend mode;
+    if (!deviceConnected) {
+        gotToSuspend();
+    }
 }
 
 void loop() {
-  bleLoop();
+    bleLoop();
 }
